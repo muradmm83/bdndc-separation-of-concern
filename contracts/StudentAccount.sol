70801d6d486eda616efcecf54c698230953699d2
Murad <muradmm83@gmail.com>
2019-07-16 22:34:29 +0300

    feat: Add operational status to StudentAccount.sol

--- contracts/StudentAccount.sol
+++ contracts/StudentAccount.sol
@@ -5,21 +5,39 @@ import "../node_modules/openzeppelin-solidity/contracts/math/SafeMath.sol";
 contract StudentAccount {
     using SafeMath for uint;
 
-    address private onlyOwner;
+    bool isOperational = true;
+    address _owner;
+
     AccountDataContract dataContract;
+    
+
+    modifier operational() {
+        require(isOperational);
+        _;
+    }
+
+    modifier isOwner() {
+        require(msg.sender == _owner);
+        _;
+    }
 
     constructor(address dataContractAddress) public {
         dataContract = AccountDataContract(dataContractAddress);
+        _owner = msg.sender;
     }
 
-    function depositToAccount() public payable {
+    function setOperational(bool status) public isOwner {
+        isOperational = status;
+    }
+    
+    function depositToAccount() public payable operational  {
         dataContract.deposit.value(msg.value)(msg.sender);
     }
 
-    function withdrawFromAccount(uint amount) public {
+    function withdrawFromAccount(uint amount) public operational {
         uint fees = amount.div(100);
         dataContract.withdraw(msg.sender, amount, fees);
-    }
+    }    
 }
 
 contract AccountDataContract {
